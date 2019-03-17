854dbf7dc45efac581980f5ad01c99514dc1481b
Yevgeniy <yevgeniyerot@gmail.com>
2019-03-17 00:08:30 -0400

    on the way

--- lib/genre.rb
+++ lib/genre.rb
@@ -12,4 +12,17 @@ class Genre
   def self.all
     @@all
   end
+
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    Song.all.each do |song|
+      puts song.first
+      # song.artist == self.artist
+    end
+  end
 end
